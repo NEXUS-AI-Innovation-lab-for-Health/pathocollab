3bbb1760cd11bc6909a9cdb49633acccd255fecb
UltimateDP <mathiasverhaeghemlipson@gmail.com>
2026-02-20 10:10:39 +0100

    fix : Alembic en sync

--- apps/cases-service/alembic/env.py
+++ apps/cases-service/alembic/env.py
@@ -1,5 +1,5 @@
 from logging.config import fileConfig
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import create_engine, engine_from_config, pool
 from alembic import context
 import os
 import sys
@@ -37,11 +37,8 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 def run_migrations_online() -> None:
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    db_url = os.getenv("ALEMBIC_DATABASE_URL") or config.get_main_option("sqlalchemy.url")
+    connectable = create_engine(db_url, poolclass=pool.NullPool)
     with connectable.connect() as connection:
         context.configure(
             connection=connection, target_metadata=target_metadata
